cb3270c8e6d508b305b8d645adb730c479f9934b
Tomasz Plewniak <t_plewniak@outlook.com>
2023-09-22 12:24:53 +0200

    Add kernel and unit tests.

---
+++ src/SharedKernel/EntityBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SharedKernel;
+
+/// <summary>
+/// A base class for DDD Entities. Includes support for domain events dispatched post-persistence.
+/// If you prefer GUID Ids, change it here.
+/// If you need to support both GUID and int IDs, change to EntityBase&lt;TId&gt; and use TId as the type for Id.
+/// </summary>
+public abstract class EntityBase
+{
+  public int Id { get; set; }
+
+  private List<DomainEventBase> _domainEvents = new();
+  [NotMapped]
+  public IEnumerable<DomainEventBase> DomainEvents => _domainEvents.AsReadOnly();
+
+  protected void RegisterDomainEvent(DomainEventBase domainEvent) => _domainEvents.Add(domainEvent);
+  internal void ClearDomainEvents() => _domainEvents.Clear();
+}
+
